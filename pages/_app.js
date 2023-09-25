7aea3ef6a02797bbf35b72456b77e5f935aff72a
Carlo Julian <71998060+hs-cjulian@users.noreply.github.com>
2023-09-25 14:27:53 -0400

    cards with random costs

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,11 @@
 import '@/styles/globals.css'
-import { Fira_Sans } from 'next/font/google'
+import { Inter } from 'next/font/google'
 
-const firaSans = Fira_Sans({ subsets: ['latin'], weight: ['400', '600', '700'] })
+const inter = Inter({ subsets: ['latin'], weight: ['400', '600', '700'] })
 
 export default function App({ Component, pageProps }) {
   return(
-    <main className={firaSans.className}>
+    <main className={inter.className}>
       <Component {...pageProps} />
     </main>
   )
--- pages/project/plan.js
+++ pages/project/plan.js
@@ -1,11 +1,21 @@
+function getRandomArbitrary(min, max) {
+  return (Math.random() * (max - min) + min).toFixed(2);
+}
+
 import styles from '@/styles/plan.module.scss'
 export async function getServerSideProps() {
   try {
-    const response = await fetch('http://127.0.0.1:3000/recommendations?postal_code=l1n9p8')
+    const response = await fetch('http://127.0.0.1:3000/recommendations?postal_code=K7C0A6')
     const json = await response.json()
+    const tasks = json.tasks.filter(t => t.name).map(t => {
+      return {
+        ...t,
+        price: getRandomArbitrary(100, 400)
+      }
+    })
     return {
       props: {
-        recommendations: json.tasks.filter(t => t.name),
+        recommendations: tasks,
       }
     }
   } catch (error) {
@@ -13,32 +23,60 @@ export async function getServerSideProps() {
   }
 }
 
-export default function Plan({ recommendations }) {
+export default function Plan({ recommendations, numToShow = 3 }) {
   return (
     <div className={styles.root}>
-      <h1>Plan</h1>
-      <div className={styles.recommendations}>
-        {JSON.stringify(recommendations, null, 2)}
-        {
-          recommendations.map((recommendation, i) => {
-            return (
-              <div className={styles.recommendation} key={i}>
-                <h2>{recommendation.name}</h2>
-              </div>
-            )
-          })
-        }
+      <div className={styles.header}>
+        <img src={'/logo.svg'} />
+        <ul className={styles.nav}>
+          <li className={styles.navItem}>Browse Categories</li>
+          <li className={styles.navItem}>Write a Review</li>
+          <li className={styles.navItem} style={{fontWeight: 600}}>Project Planner</li>
+        </ul>
+      </div>
+      <div className={styles.content}>
+        <section className={styles.sidebar}>
+          <div className={styles.welcome}>
+            Hi, 👋 Welcome to your
+            personalized planner <span className={styles.exclamation}>!</span>
+          </div>
+          <div className={styles.sidenav}>
+            <ul className={styles.sidenavItems}>
+              <li>Home Profile</li>
+              <li style={{ color: '#32527C', fontWeight: 700 }}>Project Planner</li>
+              <li>Service Requests</li>
+              <li>Saved</li>
+              <li>Account Settings</li>
+            </ul>
+          </div>
+        </section>
+        <main className={styles.main}>
+          <div className={styles.recommendations}>
+            {
+              recommendations.slice(0, numToShow).map((rec, i) => {
+                return (
+                  <button
+                    className={styles.recommendation}
+                    key={i}
+                    onClick={() => alert(`pretend this is a modal for ${rec.name}`)}
+                  >
+                    <img src={`http://localhost:3000/assets/categories/${rec.category_id}.jpg`} />
+                    <div className={styles.taskNameBenefit}>
+                      <h2 className={styles.taskName}>{rec.name}</h2>
+                      <p className={styles.benefit}>
+                        <img className={styles.sparkles} src='/sparkles.svg' />Prevents costly damage
+                      </p>
+                    </div>
+                    <div className={styles.avgCost}>
+                      ${rec.price} / <span>average cost</span>
+                    </div>
+                  </button>
 
-        {/*{*/}
-        {/*  recommendations.map((recommendation, i) => {*/}
-        {/*    return (*/}
-        {/*      <div className={styles.recommendation} key={i}>*/}
-        {/*        <h2>{recommendation.name}</h2>*/}
-        {/*      </div>*/}
-        {/*    )*/}
-        {/*  }*/}
-        {/*}*/}
-
+                )
+              })
+            }
+          </div>
+        </main>
       </div>
     </div>
   )
