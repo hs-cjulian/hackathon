9862aa3c03668d942f738945242622d35221d206
Carlo Julian <71998060+hs-cjulian@users.noreply.github.com>
2023-09-25 10:32:02 -0400

    starting styling of plan page

--- pages/project/plan.js
+++ pages/project/plan.js
@@ -1,7 +1,45 @@
-export default function Plan() {
+import styles from '@/styles/plan.module.scss'
+export async function getServerSideProps() {
+  try {
+    const response = await fetch('http://127.0.0.1:3000/recommendations?postal_code=l1n9p8')
+    const json = await response.json()
+    return {
+      props: {
+        recommendations: json.tasks.filter(t => t.name),
+      }
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export default function Plan({ recommendations }) {
   return (
-    <>
+    <div className={styles.root}>
       <h1>Plan</h1>
-    </>
+      <div className={styles.recommendations}>
+        {JSON.stringify(recommendations, null, 2)}
+        {
+          recommendations.map((recommendation, i) => {
+            return (
+              <div className={styles.recommendation} key={i}>
+                <h2>{recommendation.name}</h2>
+              </div>
+            )
+          })
+        }
+
+        {/*{*/}
+        {/*  recommendations.map((recommendation, i) => {*/}
+        {/*    return (*/}
+        {/*      <div className={styles.recommendation} key={i}>*/}
+        {/*        <h2>{recommendation.name}</h2>*/}
+        {/*      </div>*/}
+        {/*    )*/}
+        {/*  }*/}
+        {/*}*/}
+
+      </div>
+    </div>
   )
 }
--- pages/project/start.js
+++ pages/project/start.js
@@ -1,11 +1,34 @@
+import { useState, useRef, useEffect } from 'react';
+
 export default function Start() {
+  const [ postalCode, setPostalCode ] = useState("");
+  const inputElement = useRef();
+
+  useEffect(() => {
+    inputElement?.current?.focus();
+  },[])
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    window.location.assign('/project/plan?postalCode=' + postalCode);
+    return false;
+  }
+
   return (
     <div>
       <p>
         Unlock a tailored maintenance plan. Invest in the <strong>longevity and value of Your Home</strong>
       </p>
-      <form>
-        <input type="text" id="postal-code" name="postal-code" required minLength="4" maxLength="8" size="10"/>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          id="postal-code"
+          name="postal-code"
+          required minLength="6" maxLength="7"
+          onChange={(e) => setPostalCode(e.target.value) }
+          value={postalCode}
+          ref={inputElement}
+        />
         <button type={"submit"}>Get Started</button>
       </form>
     </div>
